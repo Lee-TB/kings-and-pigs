1e96e3071078ac0ac3f87dbcba39b8a06f3484b5
IndieDev <tblyduc2412@gmail.com>
2023-11-22 19:34:21 +0700

    gravity and bounce

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -8,11 +8,16 @@ export class Player {
     this.width = 100;
     this.height = 100;
 
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    this.gravity = 0.1;
+    this.bounce = 0;
+
     this.sides = {
-      top: this.position.y,
       bottom: this.position.y + this.height,
-      left: this.position.x,
-      right: this.position.x + this.width,
     };
   }
 
@@ -22,9 +27,15 @@ export class Player {
   }
 
   update(canvas) {
-    if (this.sides.bottom < canvas.height) {
-      this.position.y += 1;
+    this.position.y += this.velocity.y;
+
+    // above bottom of Canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += this.gravity;
       this.sides.bottom = this.position.y + this.height;
+    } else {
+      this.bounce = -this.velocity.y * 0.3;
+      this.velocity.y = this.bounce;
     }
   }
 }
